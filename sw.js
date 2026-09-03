2337c07c7d467c6b0d2df019d5e9ae674ad1df25
mkljl12345 <mkljl12345@users.noreply.github.com>
2026-09-03 10:03:51 +0800

    v37 seed Jeju journal with April 23 cover

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'zhijian-pwa-v36';
+const CACHE = 'zhijian-pwa-v37';
 const APP_SHELL = [
   './',
   './index.html',
@@ -8,6 +8,7 @@ const APP_SHELL = [
   './journal-v2.js',
   './journal-v2.css',
   './journal-module-texture.webp',
+  './jeju-2026-04-23-cover.png',
   './home-collage.webp',
   './home-collage-2.webp',
   './home-river-terminal.webp',
